c1b3b7cd5e0212b12d535ff3b750ac0cc1c563c4
Vinicius Caetano <vncs2009@gmail.com>
2022-12-19 19:11:56 -0300

    Update staff json

--- info_alunos.js
+++ info_alunos.js
@@ -10,32 +10,31 @@ $(async function () {
   }
 
   for (let i = 0; i < user.length; i++) {
-    fetch(`//api.github.com/users/${user[i].login}`)
-      .then(res => {
-        if (!res.ok) {
-          throw new Error(res.statusText)
-        }
-        return res.json()
-      })
-      .catch(error => {
-        console.log(error)
-      })
+    try {
+      fetch(`//api.github.com/users/${user[i].login}`)
+        .then(res => {
+          if (!res.ok) {
+            throw new Error(res.statusText)
+          }
+          return res.json()
+        })
+        //Retorna as funções obtidas via API GitHub
+        .then((result) => {
+          let { login, name, avatar_url, html_url } = result
+          //Verifica se o usuário
+          if (name == null) {
+            name = login
+          }
 
-      //Retorna as funções obtidas via API GitHub
-      .then((result) => {
-        let { login, name, avatar_url, html_url } = result
-        //Verifica se o usuário
-        if (name == null) {
-          name = login
-        }
+          $("#studentlist").append(`<div class=students id=student_${login}></div>`)
+          $(`#student_${login}`).append(`<a data-bs-toggle="modal" data-bs-target="#modal_${login}"><img class="rounded-circle" src="${avatar_url}" width="120"></a>`)
+          $(`#student_${login}`).append(`<h3>${name}</h3>`)
 
-        $("#studentlist").append(`<div class=students id=student_${login}></div>`)
-        $(`#student_${login}`).append(`<a data-bs-toggle="modal" data-bs-target="#modal_${login}"><img class="rounded-circle" src="${avatar_url}" width="120"></a>`)
-        $(`#student_${login}`).append(`<h3>${name}</h3>`)
+          /*Função para criar os Modais dinamicamente */
+          criarModais(login, name, user[i].idade, user[i].localidade, user[i].linkedIn, html_url, user[i].bio, avatar_url)
+        })
+    } catch (e) { console.log(e) }
 
-        /*Função para criar os Modais dinamicamente */
-        criarModais(login, name, user[i].idade, user[i].localidade, user[i].linkedIn, html_url, user[i].bio, avatar_url)
-      });
   }
 })
 
@@ -53,7 +52,7 @@ function criarModais(login, name, idade, localidade, linkedIn, github, bio, avat
           <p>${idade} | ${localidade}</p>
           
           <p><a href="${linkedIn}" class="modalLinks" target="_blank"><i class="bi bi-linkedin"> </i>LinkedIn</a> | <a id="url" href="${github}" class="modalLinks" target="_blank"><i class="bi bi-github"> </i>Github</a> </p>
-          <p><i class="bi bi-quote">“${bio}”</i></p>
+          <p><i class="fa-sharp fa-solid fa-quote-left"></i>${bio}<i class="fa-sharp fa-solid fa-quote-right"></i></p>
         </div>
         <div class="modal-footer">
         
